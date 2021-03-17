d2464bb7b831596436818091d390bf7c9068addd
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-03-17 16:13:35 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+  {
+    id: 1,
+    name: "Sakata Gintoki",
+    age: 29,
+    birthday: "10th October",
+    image:
+      "https://pbs.twimg.com/profile_images/731462042662494208/lhxjirl-_400x400.jpg",
+  },
+  {
+    id: 2,
+    name: "Kagura",
+    age: 14,
+    birthday: "3rd November",
+    image:
+      "https://static.wikia.nocookie.net/gintama/images/f/fa/Tfiju.png/revision/latest?cb=20161221232328",
+  },
+  {
+    id: 3,
+    name: "Hijikata Toushirou",
+    age: 27,
+    birthday: "5th May",
+    image:
+      "https://static.wikia.nocookie.net/gintama/images/b/b0/Hijikata_2017.png/revision/latest?cb=20170307114044",
+  },
+  {
+    id: 4,
+    name: "Kyo Sohma",
+    age: 17,
+    birthday: "5th January",
+    image:
+      "https://64.media.tumblr.com/5094e73a123241f6dc7b06ee266118ae/45bf7edaa86e3eee-bc/s1280x1920/d826f094cedd72decfc81b92a13e66714bae0da8.png",
+  },
+  {
+    id: 5,
+    name: "Miku Nakano",
+    age: 18,
+    birthday: "5th May",
+    image: "https://cdn.myanimelist.net/images/characters/11/366718.jpg",
+  },
+];
